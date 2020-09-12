64f67dba25775807813b48ac39badbe7c7e6db57
galica9999 <galica9999@gmail.com>
2020-09-11 21:16:45 -0400

    updated routes and buttons
    added post route to add new comment and redirect to post that comment was for.  also added button to go to comment page

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express     = require('express'),
     bodyParser  = require("body-parser"),
     mongoose    = require('mongoose'),
     Campground = require("./models/campground"),
-    seedDB = require('./seeds');
+    seedDB = require('./seeds'),
+    Comment = require('./models/comment');
 
 const { StringDecoder } = require('string_decoder');
 
@@ -68,7 +69,32 @@ app.get('/campgrounds/:id', function(req,res){
 // ===================================
 //comment routes
 app.get('/campgrounds/:id/comments/new', function(req,res){
-    res.render('comments/new');
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('comments/new', {campground:campground});
+        }
+    });
+});
+
+app.post('/campgrounds/:id/comments', function(req,res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/' + campground._id);
+                }
+            });
+        }
+    });
 })
 
 app.listen(3000, 'localhost', function () {
