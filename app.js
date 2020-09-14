332b9f1fb9bb06fe56dda582ed2073c1e273ef28
Ryan MacLennan <ram1229@rit.edu>
2020-09-14 10:01:38 -0400

    added login capability
    added login routes and login capability

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express     = require('express'), 
-    app         = express(),
-    request     = require('request'), 
-    bodyParser  = require("body-parser"),
-    mongoose    = require('mongoose'),
+var express = require('express'),
+    app = express(),
+    request = require('request'),
+    bodyParser = require("body-parser"),
+    mongoose = require('mongoose'),
     Campground = require("./models/campground"),
     seedDB = require('./seeds'),
     Comment = require('./models/comment'),
@@ -14,7 +14,7 @@ const { StringDecoder } = require('string_decoder');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 
 mongoose.connect('mongodb://localhost:27017/yelpCamp', {
     useNewUrlParser: true,
@@ -45,8 +45,8 @@ app.get('/', function (req, res) {
 })
 
 app.get("/campgrounds", function (req, res) {
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
             console.log(err);
         } else {
             res.render('campgrounds/index', { campgrounds: allCampgrounds });
@@ -58,27 +58,27 @@ app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name:name, image:image, description:description}
-    Campground.create(newCampground, function(err, result){
-        if(err){
+    var newCampground = { name: name, image: image, description: description }
+    Campground.create(newCampground, function (err, result) {
+        if (err) {
             console.log(err);
         } else {
             res.redirect('/campgrounds')
         }
     });
-    
+
 })
 
 app.get('/campgrounds/new', function (req, res) {
     res.render('campgrounds/new')
 });
 
-app.get('/campgrounds/:id', function(req,res){
-    Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground){
-        if(err){
+app.get('/campgrounds/:id', function (req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
+        if (err) {
             console.log(err);
         } else {
-            res.render('campgrounds/shows', {campground:foundCampground});
+            res.render('campgrounds/shows', { campground: foundCampground });
         }
     });
 })
@@ -86,24 +86,24 @@ app.get('/campgrounds/:id', function(req,res){
 // ===================================
 // ===================================
 //comment routes
-app.get('/campgrounds/:id/comments/new', function(req,res){
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
+app.get('/campgrounds/:id/comments/new', function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
             console.log(err);
         } else {
-            res.render('comments/new', {campground:campground});
+            res.render('comments/new', { campground: campground });
         }
     });
 });
 
-app.post('/campgrounds/:id/comments', function(req,res){
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
+app.post('/campgrounds/:id/comments', function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
             console.log(err);
             res.redirect("/campgrounds");
         } else {
-            Comment.create(req.body.comment, function(err, comment){
-                if(err){
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
                     console.log(err);
                 } else {
                     campground.comments.push(comment);
@@ -116,18 +116,18 @@ app.post('/campgrounds/:id/comments', function(req,res){
 });
 
 // AUTH routes
-app.get('/register', function(req, res){
+app.get('/register', function (req, res) {
     res.render('register');
 })
 
-app.post('/register', function(req,res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
+app.post('/register', function (req, res) {
+    var newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             return res.render('render');
         } else {
-            passport.authenticate('local')(req,res, function(){
+            passport.authenticate('local')(req, res, function () {
                 res.redirect('/campgrounds');
             });
         }
@@ -136,11 +136,16 @@ app.post('/register', function(req,res){
 })
 
 //login routes
-app.get('/login', function(req,res){
+app.get('/login', function (req, res) {
     res.render('login');
 });
 
-
+app.post('/login', passport.authenticate('local',
+    {
+        successRedirect: '/campgrounds',
+        failureRedirect: '/login'
+    }), function (req, res) {
+});
 
 
 
