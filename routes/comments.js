07fe004a62de01891d308330a9abc89d4fa2c3a9
Ryan MacLennan <ram1229@rit.edu>
2020-09-17 11:32:50 -0400

    added delete and checking for ownership

--- routes/comments.js
+++ routes/comments.js
@@ -37,7 +37,7 @@ router.post('/', isLoggedIn ,function (req, res) {
     });
 });
 
-router.get('/:comment_id/edit', function(req,res){
+router.get('/:comment_id/edit', checkCommentOwernership, function(req,res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
             res.redirect('back');
@@ -47,8 +47,24 @@ router.get('/:comment_id/edit', function(req,res){
     });
 })
 
-router.put('/:comment_id/', function(req,res){
-    res.send('meep');
+router.put('/:comment_id/', checkCommentOwernership, function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComent){
+        if (err){
+            res.redirect('back');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
+router.delete('/:comment_id', checkCommentOwernership, function(req,res){
+    Comment.findByIdAndDelete(req.params.comment_id, function(err){
+        if (err){
+            res.redirect('back');
+        } else {
+            res.redirect('back');
+        }
+    });
 });
 
 function isLoggedIn(req, res, next){
@@ -58,5 +74,24 @@ function isLoggedIn(req, res, next){
         res.redirect('/login');
     }
 }
+
+function checkCommentOwernership(req,res,next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function (err, foundComment) {
+          if (err) {
+            res.redirect("back");
+          } else {
+            if(foundComment.author.id.equals(req.user._id)){
+                next();
+            }
+            else{
+                res.redirect('back');
+            }
+          }
+        });
+      } else {
+        res.redirect('back');
+      }
+  }
 
 module.exports = router;
